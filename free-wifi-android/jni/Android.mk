03c47432c04bc7b5a86f0d3a46f875288f421043
Andriy Chaika <andriy@chaika.in.ua>
2014-05-03 21:05:28 +0300

    [android] native lib added

--- free-wifi-android/jni/Android.mk
+++ free-wifi-android/jni/Android.mk
@@ -5,4 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := FreeWifi
 LOCAL_SRC_FILES := FreeWifi.cpp
 
+LOCAL_WHOLE_STATIC_LIBRARIES := jasson_static
+
+
 include $(BUILD_SHARED_LIBRARY)
+
+$(call import-add-path, $(LOCAL_PATH)/../../libraries/jansson/proj.android)
+$(call import-module,jansson)
--- free-wifi-android/jni/FreeWifi.cpp
+++ free-wifi-android/jni/FreeWifi.cpp
@@ -1,9 +1,11 @@
 #include <jni.h>
-
+#include <jansson.h>
 extern "C"
 {
 jint Java_app_freewifi_MainActivity_addNumbers(JNIEnv*  env, jclass, jint a, jint b)
 {
+	json_error_t error;
+	json_t *root = json_loads("aaa", 0, &error);
     return a+b;
 }
 }
