05c2949169401ea377050711a0f30c7a652d4e52
Diana Sirenko <dianasirenko@gmail.com>
2014-05-05 15:19:21 +0300

    [android] add unworked version

--- free-wifi-android/src/app/freewifi/MySimpleArrayAdapter.java
+++ free-wifi-android/src/app/freewifi/MySimpleArrayAdapter.java
@@ -1,7 +1,11 @@
 package app.freewifi;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.content.Context;
 import android.graphics.Color;
+import android.net.wifi.ScanResult;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +15,22 @@ import android.widget.TextView;
 
 public class MySimpleArrayAdapter extends ArrayAdapter<String> {
   private final Context context;
-  private final String[] values;
+  private final ArrayList<ScanResult> values;
   
   private int selectedIndex;
   private int selectedColor = Color.parseColor("#1b1b1b");
 
-  public MySimpleArrayAdapter(Context context, String[] values) {
-    super(context, R.layout.rowlayout, values);
+  public MySimpleArrayAdapter(Context context, 
+		  ArrayList<ScanResult> arraylist) {
+    super(context, R.layout.rowlayout);
     this.context = context;
-    this.values = values;
+    this.values = arraylist;
   }
-
+  public void update(ArrayList<ScanResult> new_values) {
+	  values.clear();
+	  values.addAll(new_values);
+	  this.notifyDataSetChanged();
+	}
   @Override
   public View getView(int position, View convertView, ViewGroup parent) {
     
@@ -34,18 +43,18 @@ public class MySimpleArrayAdapter extends ArrayAdapter<String> {
     
 	  ImageView imageView = (ImageView) rowView.findViewById(R.id.wifiicon);
     
-	  textView.setText(values[position]);
+	  textView.setText(values.get(position).BSSID);
     
 	  // Change the icon for Windows and iPhone
-	  String s = values[position];
-	  
-		if (s.startsWith("Windows7") || s.startsWith("iPhone")
-		    || s.startsWith("Solaris")) {
-			//TODO: change imagine
-		  imageView.setImageResource(R.drawable.ic_launcher);
-		} else {
-		  imageView.setImageResource(R.drawable.ic_launcher);
-		}
+//	  String s = values[position];
+//	  
+//		if (s.startsWith("Windows7") || s.startsWith("iPhone")
+//		    || s.startsWith("Solaris")) {
+//			//TODO: change imagine
+//		  imageView.setImageResource(R.drawable.ic_launcher);
+//		} else {
+//		  imageView.setImageResource(R.drawable.ic_launcher);
+//		}
 		
 		
 
--- free-wifi-android/src/app/freewifi/WiFiListActivity.java
+++ free-wifi-android/src/app/freewifi/WiFiListActivity.java
@@ -1,50 +1,111 @@
 package app.freewifi;
+
 import java.util.ArrayList;
-import java.util.HashMap;    
-import java.util.List;    
-import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;    
-import android.content.Intent;     
-import android.content.IntentFilter;    
-import android.net.wifi.ScanResult;    
-//import android.net.wifi.WifiConfiguration;   
-import android.net.wifi.WifiManager;    
+import java.util.HashMap;
+import java.util.List;
+
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;    
-import android.os.Handler; 
-import android.util.Log;
-import android.view.ActionMode;
-import android.view.MenuItem;
+import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;    
-import android.widget.ListView;    
-import android.widget.SimpleAdapter;    
-import android.widget.TextView;    
+import android.widget.AdapterView.OnItemClickListener;  
+import android.widget.Button;
+import android.widget.ListView;       
+import android.widget.TextView;
 import android.widget.Toast;
 
+import android.app.Activity;
 import android.app.ListActivity;
-import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 
-public class WiFiListActivity extends ListActivity {
+
+public class WiFiListActivity extends Activity {
+	
+	WifiManager wifi;       
+    ListView lv;
+    TextView textStatus;
+    Button buttonScan;
+    int size = 0;
+    List<ScanResult> results;
+    MySimpleArrayAdapter adapter;
+  
+    BroadcastReceiver reciever = new BroadcastReceiver()
+    {
+        @Override
+        public void onReceive(Context c, Intent intent) 
+        {
+           results = wifi.getScanResults();
+           size = results.size();
+        }
+    };
+    IntentFilter filter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+
+    //String ITEM_KEY = "key";
+    ArrayList<ScanResult> arraylist = new ArrayList<ScanResult>();
+    //SimpleAdapter adapter;
+    
+	
   public void onCreate(Bundle icicle) {
     super.onCreate(icicle);
     
-    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-        "Linux", "OS/2" };
+//    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+//        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+//        "Linux", "OS/2" };
     
+    //get wifi list
+    setContentView(R.layout.wifilist_activity);
 
-    final Object mActionMode;
-    final int selectedItem = -1;
+    lv = (ListView)findViewById(R.id.list1);
+
+    wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+    if (wifi.isWifiEnabled() == false)
+    {
+        Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
+        wifi.setWifiEnabled(true);
+    }   
+//    this.adapter = new SimpleAdapter(WiFiListActivity.this, 
+//    		arraylist,
+//    		android.R.layout.simple_list_item_1, 
+//    		new String[] { ITEM_KEY }, new int[] { android.R.id.text1 });
     
-    MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, values);
-    setListAdapter(adapter); 
     
-    ListView lv = getListView();
+  //set adapter
+    adapter = new MySimpleArrayAdapter(this, arraylist);
+    lv.setAdapter(adapter);
     
-    lv.setItemsCanFocus(true);
+    
+    //lv.setAdapter(this.adapter);
+
+    wifi.startScan();
+    registerReceiver(reciever, filter);     
+    
+    
+    Toast.makeText(this, "Scanning....", Toast.LENGTH_SHORT).show();
+	final Handler handler = new Handler();
+	handler.postDelayed(new Runnable() {
+	  @Override
+	  public void run() {
+		  onClick();
+		  //TODO: call function
+	  }
+	}, 4000);
+    
+    
+    
+    
+    
+    
+    //MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, values);
+    //setListAdapter(adapter); 
+    
+    //ListView lv = getListView();
+    
+    //lv.setItemsCanFocus(true);
     
     OnItemClickListener listener = new OnItemClickListener()
     {
@@ -66,6 +127,49 @@ public class WiFiListActivity extends ListActivity {
     
     lv.setOnItemClickListener(listener);
   }
+  
+  public void onClick() 
+{
+    arraylist.clear();          
+    //wifi.startScan();
+
+    try 
+    {
+        size = size - 1;
+        while (size >= 0) 
+        {   
+            //add signal level
+            int rssi = results.get(size).level;
+            rssi = WifiManager.calculateSignalLevel(rssi, 5);
+            
+            
+            //if free wifi
+            String is_free = "free";
+            if(results.get(size).capabilities.contains("WEP") ||
+            		results.get(size).capabilities.contains("WPA")	||
+            		results.get(size).capabilities.contains("WPS") ||
+            		results.get(size).capabilities.contains("PSK") ||
+            		results.get(size).capabilities.contains("CCMP") ||
+            		results.get(size).capabilities.contains("TKIP"))
+            {
+            	is_free = "keyed";
+            }
+            
+            ScanResult item = results.get(size);  
+            
+            //item.put(ITEM_KEY, results.get(size).SSID + " " + rssi + results.get(size).capabilities + " " + is_free);
+            //results.get(size).SSID + "  " + results.get(size).capabilities
+
+            arraylist.add(item);
+            size--;
+            adapter.notifyDataSetChanged();   
+            
+        } 
+    }
+    catch (Exception e)
+    { }     
+    
+} 
   
   
 }
